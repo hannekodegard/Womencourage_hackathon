89096e36223c9f94360f8250477cdbda2766cc1b
Sandra Helen Husnes <sandrahh@stud.ntnu.no>
2022-09-21 14:35:34 +0300

    Current art view

---
+++ frontend/src/ArtView.tsx
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import { artPieces } from ".";
+import { AiOutlineArrowLeft, AiOutlineSearch } from "react-icons/ai";
+import { BsFillCalendarWeekFill } from "react-icons/bs";
+import { CgRuler } from "react-icons/cg";
+import { GiPaintBucket } from "react-icons/gi";
+import { IoMdHeartEmpty, IoMdHeart } from "react-icons/io";
+
+export const ArtView = ({ currentId }: { currentId: number }) => {
+  const [currentArt, setCurrentArt] = useState(artPieces[currentId - 1]);
+  const [favourite, setFavourite] = useState(false);
+
+  const clickFavourite = () => {
+    if (!favourite) {
+      setFavourite(true);
+    } else {
+      setFavourite(false);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button>
+          <AiOutlineArrowLeft />
+        </button>
+        <button>
+          <AiOutlineSearch />
+        </button>
+
+        <button onClick={clickFavourite}>
+          {favourite && favourite ? <IoMdHeart /> : <IoMdHeartEmpty />}
+        </button>
+      </div>
+      <div>
+        <h1>{currentArt.name}</h1>
+        <h5>Art #{currentArt.id}</h5>
+        <img src={currentArt.image_url} />
+        <div>
+          <h3>About</h3>
+          <h4>{currentArt.about}</h4>
+          <h5>
+            <BsFillCalendarWeekFill /> {currentArt.year}{" "}
+            <span>
+              <CgRuler /> {currentArt.height}
+            </span>{" "}
+            <span>
+              <GiPaintBucket /> {currentArt.artist}
+            </span>
+          </h5>
+        </div>
+      </div>
+    </div>
+  );
+};
